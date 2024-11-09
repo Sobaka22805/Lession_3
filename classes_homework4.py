c2d0428ff2235d8d7b7eccb6716e3fe2eed0ba0b
student <Disl_gw81@student.itstep.org>
2024-11-09 11:49:51 +0200

    Classes

--- classes_homework4.py
+++ classes_homework4.py
@@ -1,9 +1,36 @@
+# class Person:
+#    def __init__(self, name: str, surname: str, age: int):
+#        self.name = name
+#        self.surname = surname
+#        self.age = age
+#
+#    def info_person(self):
+#        print(f'Person: {self.name} | {self.surname} | {self.age}')
+
+
 class Person:
+    name: str
+    surname: str
+    age: int
+    pensione: bool
+
     def __init__(self, name: str, surname: str, age: int):
         self.name = name
         self.surname = surname
         self.age = age
+        self.set_pensione(self.age)
+
+    def set_pensione(self, value: int):
+        if value >= 60:
+            self.pensione = True
+        else:
+            self.pensione = False
 
     def info_person(self):
         print(f'Person: {self.name} | {self.surname} | {self.age}')
 
+
+Chelovek1 = Person('Абобус', 'Автобус', 68)
+Chelovek2 = Person('Амогус', 'Автобус', 45)
+print(f'Name: {Chelovek1.name}, Surname: {Chelovek1.surname}, Age: {Chelovek1.age}, Pensione: {Chelovek1.pensione}')
+print(f'Name: {Chelovek2.name}, Surname: {Chelovek2.surname}, Age: {Chelovek2.age}, Pensione: {Chelovek2.pensione}')
--- main.py
+++ main.py
@@ -2,22 +2,59 @@ import inspect
 from lesson_4 import Student
 from classes_homework4 import Person
 
-print('Lesson 5: Class Inspect')
+# print('Lesson 6: Try-Expect')
 
-print('(Student) is function: ', inspect.isfunction(Student))
-print('(Student) is class: ', inspect.isclass(Student))
-print('(Person) is function: ', inspect.isfunction(Person))
-print('(Person) is class: ', inspect.isclass(Person))
+# print('(Student) is function: ', inspect.isfunction(Student))
+# print('(Student) is class: ', inspect.isclass(Student))
+# print('(Person) is function: ', inspect.isfunction(Person))
+# print('(Person) is class: ', inspect.isclass(Person))
 
-sigStudent = inspect.signature(Student)
-sigPerson = inspect.signature(Person)
+# try:
+#   print('(Person) is method: ', inspect.ismethod(Person))
+# except Exception as exception:
+#    print(f'Error: {exception.__str__()}')
+# finally:
+#    print('Checking is done!')
 
-print(f'(Class Person)', sigPerson.parameters)
-print(f'(Class Student)', sigStudent.parameters)
-for item in sigPerson.parameters.items():
-    print('Class "Person"')
-    print(f'item - {item[1]}')
+# try:
+#    print('(Student) is method: ', inspect.ismethod(Student))
+# except Exception as exception:
+#    print(f'Error: {exception.__str__()}')
+# finally:
+#    print('Checking is done!')
 
-for item in sigStudent.parameters.items():
-    print('Class "Student"')
-    print(f'item - {item[1]}')
+# sigStudent = inspect.signature(Student)
+# sigPerson = inspect.signature(Person)
+
+# print(f'(Class Person)', sigPerson.parameters)
+# print(f'(Class Student)', sigStudent.parameters)
+# for item in sigPerson.parameters.items():
+#    print('Class "Person"')
+#    print(f'item - {item[1]}')
+
+# for item in sigStudent.parameters.items():
+#    print('Class "Student"')
+#    print(f'item - {item[1]}')
+
+
+# class ItemSoft:
+#    price: int
+#    mark: str
+#    company: str
+
+#    def __init__(self, price: int, mark: str, company: str):
+#        if type(price) is not int:
+#            raise TypeError('argument price is not int')
+#        self.price = price
+#        self.mark = mark
+#        self.company = company
+
+
+# try:
+#    keyboard = ItemSoft(1500, 'Logitech', 'Logitech International S.A')
+#    print(f'Mark: {keyboard.mark}, Price: {keyboard.price}, Company: {keyboard.company}')
+# except Exception as error:
+#    print(f'error msg: {error.__str__()}')
+# finally:
+#    print('init of instance is done')
+
